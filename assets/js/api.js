b44c42cae52458dc7d16c5683f331d5fbe38068f
Orlando Carnate <orlando.carnate@gmail.com>
2019-02-06 12:25:24 -0600

    Adjusted query to include work type and increased radius to 2km. Added a column for name and comments.

--- assets/js/api.js
+++ assets/js/api.js
@@ -7,21 +7,33 @@ $(document).ready(function () {
 
 
 
-    function filmDates(street) {
+    function filmDates() {
         var filmingURL = "https://data.cityofchicago.org/resource/k3tc-fe6r.json?"; // api endpoint
         // var transDeptPermitsURL = "https://data.cityofchicago.org/resource/erhc-fkv9.json"; // apit endpoint
 
         // FIELDS FROM BOTTOM PAGE: https://dev.socrata.com/foundry/data.cityofchicago.org/erhc-fkv9
-        filmingURL += "&applicationstatus=Open";
+        // filmingURL += "&applicationstatus=Open";
         filmingURL += "&$where=applicationenddate>='" + convertedTime + "'";
+        // filmingURL += "AND applicationstatus=Open";
         // filmingURL += "AND (streetname='LEAVITT' OR streetname='SCHOOL')"; // test value
-        filmingURL += "AND (streetname='RACINE' OR streetname='BLUE ISLAND' OR streetname='14TH')"; // WORKS! 
-        filmingURL += "&$limit=100";
-        // filmingURL += "&primarycontactlast=OPEN 4 BUSINESS PRODUCTIONS, LLC";
+        // filmingURL += "AND (streetname='RACINE' OR streetname='BLUE ISLAND' OR streetname='14TH')"; // WORKS! 
+        // filmingURL += "AND ((latitude<41.865504 OR latitude>41.861453) ";
+        // filmingURL += "AND (longitude<-87.658612 OR longitude>-87.650662))";
+        // filmingURL += "&$where=within_circle(location, 41.864527, -87.655950, 1600)";
+        filmingURL += "AND within_circle(location, 41.8645, -87.656, 2000)";
 
-        // filmingURL += "&streetname=RACINE";
+        filmingURL += "&$limit=50";
+        // filmingURL += "&primarycontactlast=OPEN 4 BUSINESS PRODUCTIONS, LLC";
+        filmingURL += "&worktypedescription='Filming'";
+        // filmingURL += "&streetname='RACINE'";
+        // filmingURL += "&streetname='BLUE ISLAND'";
+
         // filmingURL += "&streetname='" + street.toUpperCase() + "'";
-        filmingURL += "&$order=applicationstartdate ASC";
+        filmingURL += "&$order=applicationstartdate DESC";
+
+        // CHICAGO FIRE ENGINE 18 LAT, LONG: 41.864527, -87.655950
+        // NW 41.865504, -87.658612
+        // SE 41.861453, -87.650662
 
 
         // filmingURL += "&comments=1360 S BLUE ISLAND";
@@ -33,27 +45,29 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            tableGenerator(response, street);
+            tableGenerator(response);
         });
     };
 
-    function tableGenerator(response, street) {
+    function tableGenerator(response) {
         var $table = $("#table-body");
         // $table.append($("<tr class='row-fill'>").html("<h3>" + street + "</h3>"));
         for (i = 0; i < response.length; i++) {
+            var $name = $("<td>").html(response[i].applicationname);
             var start = moment(response[i].applicationstartdate, moment.ISO_8601).format("dddd, MMMM Do YYYY"); //2019-01-28T00:00:00
-            var $startDate = $("<td>").text(start);
+            var $startDate = $("<td>").html(start);
             var end = moment(response[i].applicationenddate, moment.ISO_8601).format("dddd, MMMM Do YYYY");
-            var $endDate = $("<td>").text(end);
-            var $Location = $("<td>").text(response[i].streetname + " " + response[i].suffix + ", From " + response[i].streetnumberfrom + " To " + response[i].streetnumberto);
-            $table.append($("<tr>").append($startDate, $endDate, $Location));
-            
+            var $endDate = $("<td>").html(end);
+            var $Location = $("<td>").html(response[i].streetname + " " + response[i].suffix + ", From " + response[i].streetnumberfrom + " To " + response[i].streetnumberto);
+            var $comments = $("<td>").html(response[i].comments);
+            $table.append($("<tr>").append($name, $startDate, $endDate, $Location, $comments));
+
             $("#results").append($table);
         }
         $table.append($("<tr class='row-fill'>"));
     };
 
-    filmDates("Racine and Blue Island");
+    filmDates();
     // filmDates("(Racine) OR (BLUE ISLAND)");
     // filmDates("Racine");
     // filmDates("Blue Island");
